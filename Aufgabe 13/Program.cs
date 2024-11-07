6845d0edf776c2f2e22e6cd2423dac9937d9ea37
Quentin Lorenzo <quentin_lorenzo@sluz.ch>
2024-11-07 16:37:30 +0100

    v2

--- Aufgabe 13/Program.cs
+++ Aufgabe 13/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,30 @@ namespace Aufgabe_13
     {
         static void Main(string[] args)
         {
+            int i2 = 0;
 
-            int input;
-            string[] linie = new string[input];
+            Console.WriteLine("Wie lang soll die Linie sein?");
+            Console.Write("Deine Eingabe: ");
+            uint input = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            string[,] linie = new string[input,input];
+
+            for (int i = 0; i < input; i++)
+            {
+                if (i == i2)
+                {
+                    linie[i, i2] = "*";
+                    i2++;
+                }
+                else
+                {
+                    linie[i, i2] = " ";
+                }
+            }
+            Console.WriteLine(linie);
+            Console.ReadKey();
+
         }
     }
 }
